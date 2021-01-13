6628ad508aa91d309116abe8f90fb140d04de0d8
Demetri0 <d3i0@ya.ru>
2021-01-14 04:01:42 +0600

    Clean up DefaultLayout

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,21 +1,5 @@
-import { useContext } from "react"
-
-import { UserContext } from '../../core/UserContext'
-import { LoginForm } from '../../components/LoginForm'
-
-import styles from './DefaultLayout.module.css'
-
 export function DefaultLayout({ children }) {
-    const [user] = useContext(UserContext)
-
-    if (!user) {
-        return <div className={styles.LoginBG}>
-            <LoginForm />
-        </div>
-    }
-
     return <div>
-        <div>DefaultLayout</div>
         {children}
     </div>
 }
