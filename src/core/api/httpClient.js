be4cd9693576f9af248afb63025c685e671ba3af
Demetri0 <d3i0@ya.ru>
2021-01-15 08:45:54 +0600

    Revert api url for local run

--- src/core/api/httpClient.js
+++ src/core/api/httpClient.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 export const httpClient = axios.create({
-    baseURL: 'http://194.67.90.67/api/v1'
+    baseURL: '/api/v1'
 })
 
 httpClient.interceptors.request.use(function (config) {
